cc7fa1b3b7edca17862e543720e27d825da2fd53
thersi <therese.sigmundstad@gmail.com>
2021-10-25 12:35:08 +0200

    Add MovieSearch

--- frontend/src/components/MovieSearch/index.tsx
+++ frontend/src/components/MovieSearch/index.tsx
@@ -1,44 +1,13 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 import MovieScroll from "../MovieScroll";
+import MovieFilter from "../MovieFilter";
+import SearchBar from "../SearchBar";
+import MovieAppBar from "../MovieAppBar";
 const MovieSearch = (props: any) => {
-  const [value, setValue] = useState<String>();
-  const handleSearch = (event: any) => {
-    props.click(value);
-  };
   return (
     <div>
-      <div className="searchBar">
-        <Paper
-          component="form"
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
-            width: "auto",
-            marginBottom: "50px",
-          }}
-        >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Movie"
-            inputProps={{ "aria-label": "movies" }}
-            value={value}
-            onChange={(event) => {
-              //adding the onChange event
-              setValue(event.target.value);
-            }}
-          />
-          <IconButton onClick={handleSearch}>
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </div>
-      <MovieScroll search={value} />
+      <MovieAppBar />
+      <MovieScroll />
     </div>
   );
 };
