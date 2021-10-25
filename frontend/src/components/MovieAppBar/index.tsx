a2f026e1290b16f1c32beae898bea6e46640c4f5
thersi <therese.sigmundstad@gmail.com>
2021-10-25 12:33:26 +0200

    FIx appbar component

---
+++ frontend/src/components/MovieAppBar/index.tsx
@@ -0,0 +1,29 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchBar from "../SearchBar";
+import MovieFilter from "../MovieFilter";
+
+export default function MovieAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Movies
+          </Typography>
+          <MovieFilter />
+          <SearchBar />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
