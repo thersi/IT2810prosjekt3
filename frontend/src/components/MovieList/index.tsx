1d44de2a643dc1265f6bb38af98553fbf91b329e
thersi <therese.sigmundstad@gmail.com>
2021-10-25 12:34:28 +0200

    Add dummy movielist

--- frontend/src/components/MovieList/index.tsx
+++ frontend/src/components/MovieList/index.tsx
@@ -1,15 +1,16 @@
+import { Divider } from "@mui/material";
 import React from "react";
 
 const MovieList = (props: any) => {
   return (
     <>
-      {props.movies //Det klikkes fiks imårå
-        .filter((movie: any) => movie.name === props.search)
-        .map((movie: any, index: any) => (
-          <div className="movie">
-            <img src={movie.Poster} alt="movie"></img>
-          </div>
-        ))}
+      {props.movies.map((movie: any, index: any) => (
+        <div className="movie">
+          <img src={movie.Poster} alt="movie"></img>
+          <Divider />
+          <h3>{movie.title}</h3>
+        </div>
+      ))}
     </>
   );
 };
