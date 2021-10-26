a8d47255e66192226e9a82f6e950cf05a08e0736
thersi <therese.sigmundstad@gmail.com>
2021-10-26 14:37:35 +0200

    Fix filtering of genre

--- Frontend/src/components/GenreTabs/index.tsx
+++ Frontend/src/components/GenreTabs/index.tsx
@@ -1,33 +1,104 @@
 import * as React from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import { Theme, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import "./styles.css";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const genres = [
+  "ACTION",
+  "ADVENTURE",
+  "ANIMATION",
+  "BIOGRAPHY",
+  "COMEDY",
+  "CRIME",
+  "DOCUMENTARY",
+  "DRAMA",
+  "FAMILY",
+  "FANTASY",
+  "FILMNOIR",
+  "HISTORY",
+  "HORROR",
+  "MUSIC",
+  "MUSICAL",
+  "MYSTERY",
+  "ROMANCE",
+  "SCIFI",
+  "SHORT",
+  "SPORT",
+  "THRILLER",
+  "WAR",
+  "WESTERN",
+];
+
+function getStyles(genre: string, movieGenre: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      movieGenre.indexOf(genre) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 export default function GenreTabs() {
-  const [value, setValue] = React.useState(0);
+  const theme = useTheme();
+  const [movieGenre, setMovieGenre] = React.useState<string[]>([]);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const handleChange = (event: SelectChangeEvent<typeof movieGenre>) => {
+    const {
+      target: { value },
+    } = event;
+    setMovieGenre(
+      // On autofill we get a the stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
   };
 
   return (
-    <Box sx={{ bgcolor: "background.paper" }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-        centered
-      >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
-        <Tab label="Item Four" />
-        <Tab label="Item Five" />
-        <Tab label="Item Six" />
-        <Tab label="Item Seven" />
-      </Tabs>
-    </Box>
+    <div className="genreSelect">
+      <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+        <Select
+          multiple
+          displayEmpty
+          value={movieGenre}
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <em>Select Genre</em>;
+            }
+
+            return selected.join(", ");
+          }}
+          MenuProps={MenuProps}
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem disabled value="">
+            <em>Select Genre</em>
+          </MenuItem>
+          {genres.map((genre) => (
+            <MenuItem
+              key={genre}
+              value={genre}
+              style={getStyles(genre, movieGenre, theme)}
+            >
+              {genre}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
   );
 }
