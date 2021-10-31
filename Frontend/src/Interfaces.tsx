422ab741f6c9eb1e2f3557868bbdb887f6a25fb8
thersi <therese.sigmundstad@gmail.com>
2021-10-31 12:23:59 +0100

    Move interfaces to interface file

--- Frontend/src/Interfaces.tsx
+++ Frontend/src/Interfaces.tsx
@@ -1,3 +1,8 @@
+import {
+  Dispatch,
+  SetStateAction,
+} from "react-transition-group/node_modules/@types/react";
+
 export interface Movie {
   _id: string;
   title: string;
@@ -9,4 +14,60 @@ export interface Movie {
   poster: string;
 }
 
-export interface MovieInfo {}
+export interface MovieAppBarProps {
+  handleSearch: (value: string) => void;
+  handleFilter: (value: string) => void;
+  handleSort: (value: boolean) => void;
+}
+
+export interface ThumbsByIdInput {
+  thumbsUpByIdId: string;
+}
+
+export interface ThumbsDownByIdInput {
+  thumbsDownByIdId: string;
+}
+
+export interface ThumbsUpByIdResult {
+  thumbsUpById: Movie;
+}
+
+export interface ThumbsDownByIdResult {
+  thumbsDownById: Movie;
+}
+
+export interface MovieDialogProps {
+  movie: Movie;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  setThumbsUp: Dispatch<SetStateAction<number>>;
+  thumbsUp: number;
+  setThumbsDown: Dispatch<SetStateAction<number>>;
+  thumbsDown: number;
+  voted: boolean;
+  setVoted: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface QueryMoviesResult {
+  searchAndFilter: Movie[];
+}
+
+export interface QueryMoviesInput {
+  filterGenre: string;
+  limit: number;
+  page: number;
+  order: number;
+  sortOn: string;
+  word: string;
+}
+
+export interface MovieByIdInput {
+  movieByIdId: string;
+}
+
+export interface MovieByIdResult {
+  movieById: Movie;
+}
+
+export interface MovieListProps {
+  data: QueryMoviesResult;
+}
