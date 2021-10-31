1908ed324cc2f49e78f917bf88bff61f00a50913
thersi <therese.sigmundstad@gmail.com>
2021-10-31 16:48:23 +0100

    Add label if list length 0

--- Frontend/src/components/MovieList/index.tsx
+++ Frontend/src/components/MovieList/index.tsx
@@ -6,6 +6,7 @@ const MovieList = (props: MovieListProps) => {
   const { data } = props;
   return (
     <div className="movieContainer">
+      {data.searchAndFilter.length < 1 && <p>No movies to show </p>}
       {data.searchAndFilter.map((movie: Movie) => (
         <SingleDisplay key={movie._id} {...movie} />
       ))}
