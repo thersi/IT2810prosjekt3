d7fd1dd721c64b85ea3c39bf915a48bb9de51ae7
thersi <therese.sigmundstad@gmail.com>
2021-11-01 20:01:56 +0100

    Update test on MovieFilter

--- Frontend/src/__tests__/MovieFilterTest.tsx
+++ Frontend/src/__tests__/MovieFilterTest.tsx
@@ -1,21 +1,21 @@
 import EnzymeToJson from "enzyme-to-json";
-import { configure, mount, render, shallow } from "enzyme";
+import { configure, render, shallow } from "enzyme";
+import MovieAppBar from "../components/MovieAppBar";
 import Adapter from "enzyme-adapter-react-16";
 import MovieFilter from "../components/MovieFilter";
-import "@testing-library/jest-dom";
-import MenuItem from "@mui/material/MenuItem";
-
-configure({ adapter: new Adapter() });
+import { MenuItem } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
 const component = (
   <MovieFilter
     handleSort={() => {
       return 1;
     }}
     handleFilter={() => {
-      return "title";
+      return "";
     }}
   />
 );
+configure({ adapter: new Adapter() });
 describe("Movie Filter", () => {
   it("renders correctly", () => {
     render(component);
@@ -24,24 +24,15 @@ describe("Movie Filter", () => {
     const snapshotCheck = shallow(component);
     expect(EnzymeToJson(snapshotCheck)).toMatchSnapshot();
   });
-});
-/* it("Should render correct number of menu Items", () => {
-    const shallowComp = mount(component);
-    expect(shallowComp.find("IconButton").exists()).toBeTruthy(); */
-/* expect(shallowComp.find("MenuItem")).toHaveLength(6); */
 
-/*  it("Should show the chosen menu item", () => {
+  it("Should contain correct number of elements", () => {
     const shallowComp = shallow(component);
-    expect(shallowComp.find("#ascCheck").parent).toBe(
-      shallowComp.find("#checkAsc")
-    ); */
-
-//descSort.simulate("click");
-
-/* 
-it("should contain correct components", () => {
-  expect(component.find("SearchBar").exists()).toBeTruthy();
-  expect(component.find("MovieFilter").exists()).toBeTruthy();
-  expect(component.find("MovieDialog").exists()).toBeFalsy();
+    expect(shallowComp.find(MenuItem)).toHaveLength(6);
+  });
+  /* it("Should contain correct checked components", () => {
+    const shallowComp = shallow(component);
+    const checkIconAsc = shallowComp.find("");
+    console.log(shallowComp);
+    expect(shallowComp.contains("CheckIcon")).toBeTruthy;
+  }); */
 });
- */
